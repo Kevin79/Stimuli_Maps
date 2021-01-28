21254c2ec0b7df7afb33566707f33ed3650ffdba
Kevin79 <Zayas.kevin1999@gmail.com>
2021-01-28 18:53:03 -0500

    Add files via upload

--- heatmap.py
+++ heatmap.py
@@ -8,19 +8,23 @@ from PIL import Image
 #if packages are needed use command $pip3 install <package>
 #Example: pip3 install pandas
 
-n_skip_rows = 5   #accumulative number of rows to skip
-user_file = sys.argv[1]
-user_ID = user_file[5:12]
+#to create visualization for all csv files in the directory, run $python3 heatmap.py
+#to run script for a specific file run $python heatmap.py <data file>.csv
 
-def create_plot(skip_rows):
-    print(skip_rows)
+# n_skip_rows = 5   #accumulative number of rows to skip
+# user_file = sys.argv[1]
+# user_ID = user_file[5:12]
+directory = os.fsencode("/Users/kzayas/exploration/Naturalistic_Exploration/PHP")
+
+def create_plot(user_file, skip_rows):
+    # print(skip_rows)
+    user_ID = user_file[5:12]
     cols = list(pd.read_csv(user_file, skiprows=skip_rows, nrows=1))
     stimuli = cols[0].replace(" Time","")
     print(stimuli)
 
     if stimuli == "Answer" or stimuli == "Comment":
         return stimuli
-    print(stimuli)
 
     #transpose set of rows and delete empty entries
     data = pd.read_csv(user_file, skiprows=skip_rows, nrows=4, index_col = False, usecols =[i for i in cols if "Time" not in i]).T
@@ -33,6 +37,7 @@ def create_plot(skip_rows):
     plt.ylim(-100, 100)
     plt.xlim(-100, 100)
     plt.savefig(user_ID +'.png',transparent=True)
+    plt.close()
 
     #open plot and crop
     plot = Image.open(user_ID +'.png')
@@ -56,20 +61,35 @@ def create_plot(skip_rows):
 
     return map
 
-img_list = []
+def create_multi_pdf(user_file):
+    img_list = []
+    user_ID = user_file[5:12]
+    n_skip_rows = 5     #accumulative number of rows to skip
 
-while True:
+    while True:
+        returned_value = create_plot(user_file, n_skip_rows)
 
-    returned_value = create_plot(n_skip_rows)
+        if returned_value == "Answer":
+            n_skip_rows += 2
+        elif returned_value == "Comment":
+            break
+        else:
+            img_list.append(returned_value)
+            n_skip_rows += 8
 
-    if returned_value == "Answer":
-        n_skip_rows += 2
-    elif returned_value == "Comment":
-        break
-    else:
-        img_list.append(returned_value)
-        n_skip_rows += 8
+    img = img_list[0]
+    img.save(user_ID +'.pdf', "PDF" ,resolution=100.0, save_all=True, append_images=img_list[1:]) #combine into 1 pdf
 
-#combine into 1 pdf
-img = img_list[0]
-img.save(user_ID +'.pdf', "PDF" ,resolution=100.0, save_all=True, append_images=img_list[1:])
+
+# print(len(sys.argv))
+if len(sys.argv) == 2:
+    user_file = sys.argv[1]
+    create_multi_pdf(user_file)
+else:
+    for file in os.listdir(directory):
+         filename = os.fsdecode(file)
+         if filename.endswith("csv"):
+             print(filename)
+             create_multi_pdf(filename)
+         else:
+             continue
